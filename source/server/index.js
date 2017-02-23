71753ca9511c3a495e58860d9b547b35810143e2
evenfrost <evenfrosty@gmail.com>
2017-02-23 20:58:14 +0300

    Bump to Node v7.6.0, add Koa middleware

--- source/server/index.js
+++ source/server/index.js
@@ -1,54 +1,5 @@
-const { resolve } = require('path');
-const Koa = require('koa');
-const logger = require('koa-logger');
-const conditional = require('koa-conditional-get');
-const etag = require('koa-etag');
-const error = require('koa-error');
-const bodyParser = require('koa-bodyparser');
-const methodOverride = require('koa-methodoverride');
-const convert = require('koa-convert');
-const helmet = require('koa-helmet');
-const errorHandler = require('koa-better-error-handler');
-const serve = require('koa-static');
-const PrettyError = require('pretty-error');
-const historyApiFallback = require('koa-connect-history-api-fallback');
+const app = require('./app');
 
-const app = new Koa();
-const pe = new PrettyError();
+const PORT = +process.env.PORT || 3000;
 
-app.context.onerror = errorHandler;
-
-app
-  .use(helmet())
-  .use(bodyParser())
-  .use(methodOverride())
-  .use(logger())
-  .use(convert(conditional()))
-  .use(convert(etag()))
-  .use(convert(error()));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(convert(historyApiFallback({
-    verbose: false,
-  })));
-
-  app.use(serve(resolve(__dirname, 'public')));
-}
-
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    ctx.throw(err);
-    ctx.app.emit('error', err, ctx);
-  }
-});
-
-app.on('error', err => {
-  if (!err.status || err.status >= 500) {
-    console.log(pe.render(err)); // eslint-disable-line
-  }
-});
-
-export default app;
-
+app.listen(PORT);
