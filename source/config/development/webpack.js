ee6d43a593ef3eba194f520597dfa10a35156b91
evenfrost <evenfrosty@gmail.com>
2017-05-26 13:50:20 +0300

    Reload client if HMR fails

--- source/config/development/webpack.js
+++ source/config/development/webpack.js
@@ -3,7 +3,7 @@ const { publicPath } = require('./index');
 
 module.exports = {
   entry: [
-    'webpack-hot-middleware/client',
+    'webpack-hot-middleware/client?reload=true',
   ],
   output: {
     publicPath,
